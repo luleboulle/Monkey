6bad67d7a20b6bfd13324dc03f2dda8078914f8c
Lucile Le Boullec <lu.le.boullec@gmail.com>
2018-12-03 12:15:33 +0100

    hello

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 module.exports = {
-    database: '',
-    username: '',
-    password: '',
+    database: process.env.MYSQL_ADDON_DB,
+    username: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
     options: {
         host: 'localhost',
         dialect: 'mysql'
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express()
-//const models = require('./models/index');
+const models = require('./models/index');
 
 // Decode json and x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -11,12 +11,10 @@ app.use(bodyParser.json())
 // Add a bit of logging
 app.use(morgan('short'))
 
-app.get('/', function(req, res) {
-  res.send('Hello world')
-})
+
 
 // Get all the users defined
-/*app.get('/', function (req, res) {
+app.get('/', function (req, res) {
   models.User.findAll()
     .then((users) => {
       res.json(users)
@@ -31,10 +29,10 @@ app.post('/', function(req, res) {
     .then(() => {
       res.send('User added !')
     })
-})*/
+})
 
 // Synchronize models
-//models.sequelize.sync().then(function() {
+models.sequelize.sync().then(function() {
   /**
    * Listen on provided port, on all network interfaces.
    * 
@@ -45,4 +43,4 @@ app.listen(process.env.PORT, function() {
       console.log('Express server listening on port 3000' + process.env.PORT);
       
   });
-//});
+});
